754099a739102befc8c82b050b859c5f845a8b86
Nicholas Hobbs <nick.hobbs42@gmail.com>
2020-07-05 17:53:53 +1000

    Separate packet construction from main code

--- sockets.py
+++ sockets.py
@@ -1,10 +1,31 @@
 #!/usr/bin/python3
 import socket, sys
 
-s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((sys.argv[1], 502))
-x = (0x0001000000060103c34f0002).to_bytes(12, byteorder='big')
-print(x)
-s.sendall(x)
-print(s.recv(1000))
+"""
+Construct the bytes array to send in order to poll holding registers.
+Function code: 0x03
+"""
+def constructPollHoldingRegisters(unitID, address, length):
+  transactionIdentifier = (0x0001).to_bytes(2, byteorder='big')
+  protocolIdentifier = (0x0000).to_bytes(2, byteorder='big')
+  remainingPacketLength = (6).to_bytes(2, byteorder='big')
+  functionCode = (0x03).to_bytes(1, byteorder='big')
+
+  outPacket = transactionIdentifier + protocolIdentifier + remainingPacketLength
+  outPacket += unitID.to_bytes(1, byteorder='big')
+  outPacket += functionCode
+  outPacket += (address-1).to_bytes(2, byteorder='big')
+  outPacket += length.to_bytes(2, byteorder='big')
+  return outPacket
+
+
+
+if __name__ == "__main__":
+  s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  s.connect((sys.argv[1], 502))
+  y = (0x0001000000060103c34f0002).to_bytes(12, byteorder='big')
+  x = constructPollHoldingRegisters(1, 50000, 2)
+  print(y,x)
+  s.sendall(x)
+  print(s.recv(1000))
 
