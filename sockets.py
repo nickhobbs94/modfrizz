cc26b939a7cfe609c03e1f7939db9b28895854c4
Nicholas Hobbs <nick.hobbs42@gmail.com>
2020-07-05 20:41:59 +1000

    Parse returned header and print payload

--- sockets.py
+++ sockets.py
@@ -6,7 +6,7 @@ Construct the bytes array to send in order to poll holding registers.
 Function code: 0x03
 """
 def constructPollHoldingRegisters(unitID, address, length):
-  transactionIdentifier = (0x0001).to_bytes(2, byteorder='big')
+  transactionIdentifier = (0x0001).to_bytes(2, byteorder='big') #TODO: increment this
   protocolIdentifier = (0x0000).to_bytes(2, byteorder='big')
   remainingPacketLength = (6).to_bytes(2, byteorder='big')
   functionCode = (0x03).to_bytes(1, byteorder='big')
@@ -18,14 +18,33 @@ def constructPollHoldingRegisters(unitID, address, length):
   outPacket += length.to_bytes(2, byteorder='big')
   return outPacket
 
+def printRegisters(payload):
+  for a,b in zip(payload[::2], payload[1::2]):
+    print(hex(a*0x100 + b))
 
+def parseHeader(inPacket):
+  packetLength = len(inPacket)
+
+  transactionIdentifier = int.from_bytes(inPacket[:2], byteorder='big')
+  protocolIdentifier = int.from_bytes(inPacket[2:4], byteorder='big')
+  remainingLength = int.from_bytes(inPacket[4:6], byteorder='big')
+
+  unitID = int.from_bytes(inPacket[6:7], byteorder='big')
+  functionCode = int.from_bytes(inPacket[7:8], byteorder='big')
+  byteCount = int.from_bytes(inPacket[8:9], byteorder='big')
+
+  return(inPacket[-byteCount:])
 
 if __name__ == "__main__":
   s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   s.connect((sys.argv[1], 502))
-  y = (0x0001000000060103c34f0002).to_bytes(12, byteorder='big')
   x = constructPollHoldingRegisters(1, 50000, 2)
-  print(y,x)
   s.sendall(x)
-  print(s.recv(1000))
+  print(x)
+  inPacket = s.recv(1000)
+  print(inPacket)
+  
+  payload = parseHeader(inPacket)
+  printRegisters(payload)
+
 
